7d6e4ce5d93228a9fb56281abe8d1eb6c0d76b07
KBhanuDeepthi <bhanudeepthivinni915@gmail.com>
2022-09-22 22:28:11 +0530

    Added:Swap number logic

---
+++ Swap_num.java
@@ -0,0 +1,19 @@
+class Swap_num {
+
+	public static void main(String[] args) {
+		int x=10;
+		int y=20;
+		System.out.println("Before Swapping");
+		System.out.println("x=" +x);
+		System.out.println("y=" +y);
+		x=x+y;
+		y=x-y;
+		x=x-y;
+		System.out.println("After Swapping");
+		System.out.println("x=" +x);
+		System.out.println("y=" +y);
+		
+
+	}
+
+}
